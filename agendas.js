ba8a729b592b3d9f28d00e08e6d9b058d075502e
Maxwell Karren <max@chuckwalladesign.com>
2016-12-29 10:23:07 -0700

    Add files via upload

---
+++ agendas.js
@@ -0,0 +1,15 @@
+// Click to expand
+$(".click.click-14").click(function(){
+	$(".expand.expand-14").slideToggle();
+	$('.click-14 .fa').toggleClass('fa-angle-down fa-angle-right');
+});
+
+$(".click.click-15").click(function(){
+	$(".expand.expand-15").slideToggle();
+	$('.click-15 .fa').toggleClass('fa-angle-down fa-angle-right');
+});
+
+$(".click.click-16").click(function(){
+	$(".expand.expand-16").slideToggle();
+	$('.click-16 .fa').toggleClass('fa-angle-down fa-angle-right');
+});
